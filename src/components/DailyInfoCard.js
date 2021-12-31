fdceaee8f240879b9d466bf38f5fa67e8c3ada3f
M Emin Özdoğan <m.eminozdogan@hotmail.com>
2021-12-31 21:58:55 +0300

    just connected some json

---
+++ src/components/DailyInfoCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from '../style/dailyInfoCard.module.css'
+import data from '../data/dailyCard.json'
+function DailyInfoCard() {
+    console.log(data);
+
+    return (
+        <h2>daily card js </h2>
+    )
+}
+
+export default DailyInfoCard
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import DailyInfoCard from '../components/DailyInfoCard'
 import Header from '../components/Header'
+import style from '../style/home.module.css'
 
 function Home() {
     return (
         <>
             <Header />
+            <div className={style.home_container}>
+                <DailyInfoCard />
+            </div>
+
         </>
     )
 }
