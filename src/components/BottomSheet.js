7255b778cc95346bc9210b1c778ef9a7a6f1e414
M Emin Özdoğan <m.eminozdogan@hotmail.com>
2022-01-02 18:47:38 +0300

    added swipeable bottom sheet but need some fix

---
+++ src/components/BottomSheet.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import SwipeableBottomSheet from 'react-swipeable-bottom-sheet'
+import style from '../style/bottomSheet.module.css'
+import StartRoom from './bottom_sheets/StartRoom'
+
+export default function BottomSheet(props) {
+
+    return (
+        <SwipeableBottomSheet
+            open={props.sheetVisible}
+            onChange={() => {
+                props.setSheetVisible(!props.sheetVisible)
+                props.setItemsVisible(true)
+            }}
+            fullScreen={props.sheetTitle === 'room detail' ? true : false}
+        >
+            <div className={style.bottomSheetContainer}
+                style={{ backgroundColor: props.sheetTitle === 'profile' ? 'transparent' : '' }}
+            >
+                <StartRoom
+                    setSheetCreateRoom={props.setSheetCreateRoom}
+                    setSheetVisible={(item) => {
+                        props.setSheetVisible(item)
+                        props.setItemsVisible(true)
+                    }}
+                />
+            </div>
+        </SwipeableBottomSheet >
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DailyInfoCard from '../components/DailyInfoCard'
 import Header from '../components/Header'
 import RoomInfoCard from '../components/RoomInfoCard'
 import style from '../style/home.module.css'
 import { AiOutlinePlus } from "react-icons/ai";
 import { BsFillGrid3X3GapFill } from "react-icons/bs";
+import data from '../data/roomCard.json'
+import BottomSheet from '../components/BottomSheet'
 
 function Home() {
+    const [itemsVisible, setItemsVisible] = useState(true)
+    const [sheetVisible, setSheetVisible] = useState(false)
+    const [sheetCreateRoom, setSheetCreateRoom] = useState(false)
+    const [loaderVisibility, setLoaderVisibility] = useState(false)
+    const [cardId, setCardId] = useState(1)
     return (
         <>
             <Header />
@@ -15,7 +22,7 @@ function Home() {
                 <RoomInfoCard />
             </div>
             <div className={style.action_btn}>
-                <button>
+                <button onClick={() => setSheetVisible(true)}>
                     <AiOutlinePlus className='mr-2' />
                     Start a room
                 </button>
@@ -23,7 +30,20 @@ function Home() {
                     <BsFillGrid3X3GapFill />
                 </button>
             </div>
-
+            <BottomSheet
+                sheetTitle='start room'                
+                setSheetVisible={(item) => setSheetVisible(item)}
+                sheetVisible={sheetVisible}
+                cardDetail={data.find((item) => item.id === cardId)}
+                setItemsVisible={(item) => setItemsVisible(item)}
+                setSheetCreateRoom={(item) => {
+                    setLoaderVisibility(true)
+                    setTimeout(() => {
+                        setSheetCreateRoom(item)
+                        setLoaderVisibility(false)
+                    }, 1000);
+                }}
+            />
         </>
     )
 }
